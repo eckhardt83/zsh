34071f1b304486e95674f28cff9448d45643ca8c
eckhardt83 <eckhardt83@gmail.com>
2024-05-08 13:38:23 +0200

    Add files via upload

--- install-zsh.sh
+++ install-zsh.sh
@@ -2,59 +2,58 @@
 
 # Funktion til at installere Zsh på Ubuntu
 function install_zsh_ubuntu {
-	echo "Installerer zsh..."
-    sudo apt update
-    sudo apt install -y zsh
+    echo "Installerer Zsh på Ubuntu..."
+    sudo apt update && sudo apt install -y zsh
 }
 
 # Funktion til at installere Zsh på Fedora
 function install_zsh_fedora {
-	echo "Installerer zsh..."
+    echo "Installerer Zsh på Fedora..."
     sudo dnf install -y zsh
 }
 
 # Funktion til at installere Zsh på Arch Linux
 function install_zsh_arch {
-	echo "Installerer zsh..."
+    echo "Installerer Zsh på Arch Linux..."
     sudo pacman -Sy --noconfirm zsh
 }
 
 # Funktion til at installere Zsh på NixOS
 function install_zsh_nixos {
-	echo "Installerer zsh..."
+    echo "Installerer Zsh på NixOS..."
     sudo nix-env -iA nixos.zsh
 }
 
-# Funktion til at installere Zsh plugins
+# Funktion til at installere Zsh-plugins
 function install_zsh_plugins {
-	echo "Installerer zsh-plugins..."
-	./install-zsh-plugins.sh
+    echo "Installerer Zsh-plugins..."
+    ./install-zsh-plugins.sh
 }
 
-# Funktion til at installere Zsh theme
+# Funktion til at installere Zsh-theme
 function install_zsh_theme {
-	echo "Installerer zsh-theme..."
-	./install-zsh-theme.sh
+    echo "Installerer Zsh-theme..."
+    ./install-zsh-theme.sh
 }
 
-# Funktion til at kopiere Zsh config Powerline10k config
+# Funktion til at kopiere Zsh konfiguration og Powerline10k konfiguration
 function copy_zsh_config {
-	echo "kopiere Zsh config & Powerline10k config..."
-	cp ./zshrc ~/.zshrc
-	cp ./p10k.zsh ~/.p10k.zsh
+    echo "Kopierer Zsh konfiguration..."
+    cp ./zshrc ~/.zshrc
+    #cp ./p10k.zsh ~/.p10k.zsh
 }
 
 # Funktion til at installere FiraCode Nerd Font
 function install_firacode_nerd_font {
+    echo "Installerer FiraCode Nerd Font..."
     # Opret en midlertidig mappe for fontene
     mkdir -p ~/.local/share/fonts
 
     # Download og installer FiraCode Nerd Font
-    wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-    unzip -o ~/.local/share/fonts/FiraCode.zip -d ~/.local/share/fonts
+    wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip &&
+    unzip -o ~/.local/share/fonts/FiraCode.zip -d ~/.local/share/fonts &&
     fc-cache -f -v ~/.local/share/fonts
 }
-
 
 # Identificer hvilken Linux-distribution der kører ved at læse /etc/os-release
 if [ -r /etc/os-release ]; then
@@ -73,19 +72,15 @@ fi
 # Installer Zsh og FiraCode Nerd Font baseret på distributionen
 case $distro in
     ubuntu)
-        echo "Installerer Zsh på Ubuntu..."
         install_zsh_ubuntu
         ;;
     fedora)
-        echo "Installerer Zsh på Fedora..."
         install_zsh_fedora
         ;;
     arch)
-        echo "Installerer Zsh på Arch Linux..."
         install_zsh_arch
         ;;
     nixos)
-        echo "Installerer Zsh på NixOS..."
         install_zsh_nixos
         ;;
     *)
@@ -94,13 +89,17 @@ case $distro in
         ;;
 esac
 
+# Installer Zsh-plugins, tema og kopier konfigurationer
 install_zsh_plugins
 install_zsh_theme
 copy_zsh_config
+cp ./p10k.zsh ~/.p10k.zsh
 
 # Installer FiraCode Nerd Font på alle distributioner
-echo "Installerer FiraCode Nerd Font..."
 install_firacode_nerd_font
 
-echo "Zsh er blevet installeret. Skift til Zsh med kommandoen: chsh -s \$(which zsh)"
-sudo chsh -s $(which zsh) $USER
+# Skift standard shell til Zsh
+echo "Skifter standard shell til Zsh..."
+chsh -s $(which zsh) $USER
+
+echo "Færdig!"
