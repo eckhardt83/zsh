1b03c384dbe9898847a1a7878c286031e860dce6
eckhardt83 <eckhardt83@gmail.com>
2024-05-08 14:36:55 +0200

    Add files via upload

--- install-zsh.sh
+++ install-zsh.sh
@@ -27,13 +27,53 @@ function install_zsh_nixos {
 # Funktion til at installere Zsh-plugins
 function install_zsh_plugins {
     echo "Installerer Zsh-plugins..."
-    sudo ./install-zsh-plugins.sh
+	function install_plugins {
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.config/zsh}/plugins/zsh-autosuggestions
+    git clone https://github.com/fdellwing/zsh-bat.git ${ZSH_CUSTOM:-~/.config/zsh}/plugins/zsh-bat
+    git clone https://github.com/ael-code/zsh-colored-man-pages.git ${ZSH_CUSTOM:-~/.config/zsh}/plugins/zsh-colored-man-pages
+    git clone https://github.com/Freed-Wu/zsh-colorize-functions.git ${ZSH_CUSTOM:-~/.config/zsh}/plugins/zsh-colorize-functions
+    git clone https://github.com/qoomon/zsh-lazyload.git ${ZSH_CUSTOM:-~/.config/zsh}/plugins/zsh-lazyload
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.config/zsh}/plugins/zsh-syntax-highlighting
+    echo "Plugins er installeret, have a nice day :)"
+	}
+ 
+	DIR=~/.config/zsh/plugins
+
+	# Kontroller om plugin-mappen allerede findes
+	if [ -d "$DIR" ]; then
+		echo "Plugin-mappen eksisterer allerede. Installerer plugins..."
+		install_plugins
+	else
+		echo "Opretter plugin-mappen..."
+		mkdir -p ~/.config/zsh/plugins
+		echo "Installerer plugins..."
+		install_plugins
+	fi
 }
 
 # Funktion til at installere Zsh-theme
 function install_zsh_theme {
     echo "Installerer Zsh-theme..."
-    sudo ./install-zsh-theme.sh
+    # Funktion til at installere et tema (f.eks. Powerlevel10k)
+	function install_theme {
+		local theme_dir="${ZSH_CUSTOM:-~/.config/zsh}/theme"
+		echo "Installing Powerlevel10k theme..."
+		git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$theme_dir/powerlevel10k"
+		echo "Powerlevel10k theme er installeret."
+	}
+
+	# Kontroller om tema-mappen eksisterer
+	theme_dir="$HOME/.config/zsh/theme"
+	if [ -d "$theme_dir" ]; then
+		echo "Plugin-mappen eksisterer."
+		install_theme
+	else
+		echo "Opretter plugin-mappe..."
+		mkdir -p "$theme_dir"
+		echo "Plugin-mappen er oprettet."
+		install_theme
+fi
+
 }
 
 # Funktion til at kopiere Zsh konfiguration og Powerline10k konfiguration
